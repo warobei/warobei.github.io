d1f968cb5ba86c989793d9c41194967189ab30cf
warobei <46086956+warobei@users.noreply.github.com>
2020-07-27 00:17:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function () {
-
-$('.slider__inner, .news__slider-inner').slick({
+  $(".slider__inner, .news__slider-inner").slick({
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     infinite: false,
+  });
 
-});
+  $("input, select").styler();
 
-$('input, select').styler();
-
-
+  $(".header__burger").on("click", function () {
+    $(".menu ul").slideToggle();
+  });
 });
